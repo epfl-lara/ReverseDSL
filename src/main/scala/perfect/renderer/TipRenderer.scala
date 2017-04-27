016708e29e6b0974e5abe7692a1a3bb0137907a9
Mikaël Mayer <mikael.mayer@epfl.ch>
2017-04-27 16:18:07 +0200

    Added tip renderer

---
+++ src/main/scala/perfect/renderer/TipRenderer.scala
@@ -0,0 +1,33 @@
+package perfect
+package renderer
+import inox._
+import inox.trees._
+
+/**
+  * Created by Mikael on 27/04/2017.
+  */
+object TipRenderer extends inox.ast.Printer {
+  val trees = inox.trees
+  implicit val symbols = {
+    Utils.defaultSymbols.withFunctions(ReverseProgram.funDefs)
+  }
+  lazy val context = Context.empty.copy(options = Options(Seq(optSelectedSolvers(Set("smt-cvc4")))))
+  lazy val prog = InoxProgram(context, symbols)
+
+  def printTip(e: Expr) = {
+    val sc = new java.io.StringWriter()
+    val tipPrinter = new inox.tip.Printer(prog, sc)
+    tipPrinter.printScript(e)
+    var res = sc.toString
+    if(res.startsWith("(assert ")) {
+      res = res.substring("(assert ".length, res.length - "(assert )".length)
+    }
+    res
+  }
+
+  override protected def ppBody(tree: Tree)(implicit ctx: PrinterContext): Unit = tree match {
+    case e: Expr =>
+      p"${printTip(e)}"
+    case _ => super.ppBody(tree)
+  }
+}
