420f1135603f913378715ad2ae251d762427ccb3
Mikaël Mayer <mikael.mayer@epfl.ch>
2017-05-03 13:45:56 +0200

    Test passing for more precise operators.

--- src/test/scala/perfect/RecursiveTest.scala
+++ src/test/scala/perfect/RecursiveTest.scala
@@ -14,22 +14,22 @@ class RecursiveTest extends FunSuite with TestHelpers {
   import InoxConvertible._
   import Utils._
   import StringConcatExtended._
+  implicit val symbols = Utils.defaultSymbols
 
-  test("Home-made list map") {
-    val mpDef = ReverseProgram.RecLens.build("map",
-      "l"::inoxTypeOf[List[String]], "f"::inoxTypeOf[String => String])(inoxTypeOf[List[String]])((mprec, ls, f) =>
-        if_(ls.isInstOf(TCons[String])) {
-          let("c"::TCons[String], ls.asInstOf(TCons[String]))(c =>
-            let("head"::String, c.getField(head))( head =>
-              ADT(TCons[String], Seq(Application(f, Seq(head)), mprec(c.getField(tail), f)))
-            )
-          )
-        } else_ {
-          _List[String]()
-        })
+  val mapDef = ReverseProgram.RecLens.build("map",
+    "l"::inoxTypeOf[List[String]], "f"::inoxTypeOf[String => String])(inoxTypeOf[List[String]])((mprec, ls, f) =>
+    if_(ls.isInstOf(TCons[String])) {
+      let("c"::TCons[String], ls.asInstOf(TCons[String]))(c =>
+        let("head"::String, c.getField(head))( head =>
+          ADT(TCons[String], Seq(Application(f, Seq(head)), mprec(c.getField(tail), f)))
+        )
+      )
+    } else_ {
+      _List[String]()
+    })
 
-
-    val prog = let("mp"::inoxTypeOf[(List[String], String => String) => List[String]], mpDef)(mp =>
+  test("Home-made list map inverse list") {
+    val prog = let("mp"::inoxTypeOf[(List[String], String => String) => List[String]], mapDef)(mp =>
       mp(_List[String]("brother", "boss"), \("s"::String)(s => "Big " +& s))
     )
     prog shouldProduce _List[String]("Big brother", "Big boss")
@@ -37,6 +37,38 @@ class RecursiveTest extends FunSuite with TestHelpers {
       case Let(mpd, mpval, Application(mpv, Seq(l, Lambda(Seq(i), StringConcat(StringLiteral("Big "), iv))))) =>
         mpv shouldEqual mpd.toVariable
         l shouldEqual _List[String]("brothers", "boss")
+    }
+  }
+  test("Home-made list map inverse precise list modification") {
+    val prog = let("mp"::inoxTypeOf[(List[String], String => String) => List[String]], mapDef)(mp =>
+      mp(_List[String]("brother", "boss"), \("s"::String)(s => "Big " +& s))
+    )
+    val original = _List[String]("Big brother", "Big boss")
+    val modified = ProgramFormula.StringInsert.Expr("Big brother", "s", "", ProgramFormula.AssociativeInsert.InsertAutomatic)
+    prog shouldProduce original
+    prog repairFrom
+      ProgramFormula.TreeModification(
+        inoxTypeOf[List[String]],
+        inoxTypeOf[String],
+        original,
+        modified,
+        List(Utils.head)
+      ) match {
+      case Let(mpd, mpval, Application(mpv, Seq(l, Lambda(Seq(i), StringConcat(StringLiteral("Big "), iv))))) =>
+        mpv shouldEqual mpd.toVariable
+        l shouldEqual _List[String]("brothers", "boss")
+    }
+  }
+  test("Home-made list map inverse function") {
+    val prog = let("mp"::inoxTypeOf[(List[String], String => String) => List[String]], mapDef)(mp =>
+      mp(_List[String]("brother", "boss"), \("s"::String)(s => "Big " +& s))
+    )
+    prog shouldProduce _List[String]("Big brother", "Big boss")
+    prog repairFrom _List[String]("Great brother", "Big boss") shouldProduce
+    _List[String]("Great brother", "Great boss") match {
+      case Let(mpd, mpval, Application(mpv, Seq(l, Lambda(Seq(i), StringConcat(StringLiteral("Great "), iv))))) =>
+        mpv shouldEqual mpd.toVariable
+        l shouldEqual _List[String]("brother", "boss")
     }
   }
 }
