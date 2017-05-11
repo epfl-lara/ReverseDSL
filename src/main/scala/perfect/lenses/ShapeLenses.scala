6deabadc6cf24a6691ad366478328a87a20b49d9
Mikaël Mayer <mikael.mayer@epfl.ch>
2017-05-11 11:41:42 +0200

    Migrated TreeUnwrapLenses

--- src/main/scala/perfect/lenses/ShapeLenses.scala
+++ src/main/scala/perfect/lenses/ShapeLenses.scala
@@ -1,13 +1,13 @@
 package perfect.lenses
 
 import perfect.InoxProgramUpdater
-import perfect.core.predef.TreeModificationLenses
+import perfect.core.predef.{TreeModificationLenses, TreeUnwrapLenses}
 
 /**
   * Created by Mikael on 09/05/2017.
   */
 trait ShapeLenses
   extends perfect.lenses.TreeWrapLenses
-  with perfect.lenses.TreeUnwrapLenses
+  with TreeUnwrapLenses
   with TreeModificationLenses { self: InoxProgramUpdater.type =>
 }
